bc845518a4ccb1417ce1b4ed93e5a8da621af466
Willian Oliveira <wijloc@gmail.com>
2020-11-02 13:12:49 -0300

    feat: like repository from repositories list

--- src/App.js
+++ src/App.js
@@ -22,7 +22,19 @@ export default function App() {
   
 
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`);
+
+    const likedRepository = response.data;
+
+    const repositoriesUpdated = repositories.map( repository => {
+      if (repository.id === id){
+        return likedRepository;
+      } else {
+        return repository;
+      }
+    })
+
+    setRepositories(repositoriesUpdated);
   }
 
   return (
